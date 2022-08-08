e8d7498cd3bc80fa0b8bf2c78c2dd0eec7bcd840
Sarita Shera <sarita.shera@gmail.com>
2022-08-07 22:47:13 -0700

    Finished CRUD course.

---
+++ firststepcms/core/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Core
+
+# Register your models here.
+@admin.register(Core)
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {
+        "slug": ("title",),
+    }
---
+++ firststepcms/core/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from .models import Core
+from django.views.generic import (
+    ListView,
+    DetailView,
+    UpdateView,
+    CreateView,
+    DeleteView,
+)
+from django.urls import reverse_lazy
+
+# Create your views here.
+class IndexView(ListView):
+    model = Core
+    template_name = "core/index.html"
+    context_object_name = "index"
+
+
+class SingleView(DetailView):
+    model = Core
+    template_name = "core/single.html"
+    context_object_name = "post"
+
+
+class PostsView(ListView):
+    model = Core
+    template_name = "core/posts.html"
+    context_object_name = "post_list"
+
+
+class AddView(CreateView):
+    model = Core
+    template_name = "core/add.html"
+    fields = "__all__"
+    success_url = reverse_lazy("core:posts")
+
+
+class EditView(UpdateView):
+    model = Core
+    template_name = "core/edit.html"
+    fields = "__all__"
+    pk_url_kwarg = "pk"
+    success_url = reverse_lazy("core:posts")
+
+
+class Delete(DeleteView):
+    model = Core
+    template_name = "core/confirm-delete.html"
+    pk_url_kwarg = "pk"
+    success_url = reverse_lazy("core:posts")
